df5eea508a95a19134a17a2c814b23480d610c97
Jędrzej <Jdrzej@DESKTOP-UV4BOJD>
2025-10-20 17:47:40 +0200

    First migration and inheritance after WorkItem

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -8,6 +8,9 @@ namespace MyBoards.Entities
             
         }
         public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<Issue> Issue{ get; set; }
+        public DbSet<Epic> Epic{ get; set; }
+        public DbSet<Task> Task{ get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Comment> Comments { get; set; }
@@ -16,15 +19,25 @@ namespace MyBoards.Entities
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Epic>()
+                .Property(wi => wi.EndDate)
+                .HasPrecision(3);
+
+            modelBuilder.Entity<Task>()
+                .Property(wi => wi.Activity)
+                .HasMaxLength(200);
+            modelBuilder.Entity<Task>()
+                .Property(wi => wi.RemaningWork)
+                .HasPrecision(14, 2);
+            
+            modelBuilder.Entity<Issue>()
+                .Property(wi => wi.Efford)
+                .HasColumnType("decimal(5,2)");
+
             modelBuilder.Entity<WorkItem>(eb =>
             {
                 eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
                 eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
-                eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
-                eb.Property(wi => wi.EndDate).HasPrecision(3);
-                eb.Property(wi => wi.Activity).HasMaxLength(200);
-                eb.Property(wi => wi.RemaningWork).HasPrecision(14, 2);
-
                 eb.Property(wi => wi.Priority).HasDefaultValue(1);
 
                 eb.HasMany(w => w.Comments)
--- MyBoards/Entities/WorkItem.cs
+++ MyBoards/Entities/WorkItem.cs
@@ -4,32 +4,34 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyBoards.Entities
 {
-    public class WorkItem
+    public class Epic : WorkItem
     {
-        public int Id { get; set; }
-        //[Required]
-        //public string State { get; set; }
-
-        //[Column(TypeName = "varchar200")]
-        public string Area { get; set; }
-
-        //[Column("Iteration_Path")]
-        public string IterationPath { get; set; }
-        public int Priority { get; set; }
-        // Epic
-        public DateTime? StartDate {  get; set; }
+        public DateTime? StartDate { get; set; }
         //[Precision(3)]
-        public DateTime? EndDate {  get; set; }
-        // Issue
+        public DateTime? EndDate { get; set; }
+    }
+
+    public class Issue : WorkItem
+    {
         //[Column(TypeName = "decimal(5,2)")]
         public decimal Efford {  get; set; }
-        // Task
+    }
+
+    public class Task: WorkItem
+    {
         //[MaxLength(200)]
         public string Activity { get; set; }
-
         //[Precision(14,2)]
         public decimal RemaningWork { get; set; }
-        public string Type { get; set; }
+    }
+    public abstract class WorkItem
+    {
+        public int Id { get; set; }
+        //public WorkItem State { get; set; }
+        public string Area { get; set; }
+
+        public string IterationPath { get; set; }
+        public int Priority { get; set; }
 
         public List<Comment> Comments { get; set; } = new List<Comment>();
 
